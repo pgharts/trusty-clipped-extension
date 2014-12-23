faf4dac49828ea4891e399f826ec45e7964d4ddc
Eric Sipple <saalon@gmail.com>
2014-12-23 12:25:43 -0500

    1.0.5

--- clipped_extension.rb
+++ clipped_extension.rb
@@ -1,7 +1,7 @@
 require 'trusty-clipped-extension'
 require 'acts_as_list'
 require 'uuidtools'
-require 'cloud'
+require 'trusty_cms_clipped_extension/cloud'
 require 'paperclip'
 require 'will_paginate/array'
 
