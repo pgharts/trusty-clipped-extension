aecb5a3c1afba4863b368e975d41ed0c97a00654
Eric Sipple <saalon@gmail.com>
2014-12-29 10:55:00 -0500

    Fixing admin assets manifest.

--- app/assets/javascripts/admin/assets_admin.js
+++ app/assets/javascripts/admin/assets_admin.js
@@ -10,7 +10,7 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
-//= require jquery.ui.widget
-//= require jquery.iframe-transport
-//= require jquery.fileupload
-//= require assets
+//= require 'admin/jquery.ui.widget'
+//= require 'admin/jquery.iframe-transport'
+//= require 'admin/jquery.fileupload'
+//= require 'admin/assets'
