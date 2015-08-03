36160d0d75b4b8f4f16b9cbdff957e0320a5f599
Eric Sipple <saalon@gmail.com>
2015-08-03 12:13:45 -0400

    2.0.1-beta

--- lib/trusty-clipped-extension.rb
+++ lib/trusty-clipped-extension.rb
@@ -1,5 +1,5 @@
 module TrustyCmsClippedExtension
-  VERSION     = "2.0.0-beta"
+  VERSION     = "2.0.1-beta"
   SUMMARY     = %q{Assets for TrustyCms CMS}
   DESCRIPTION = %q{Asset-management derived from Keith Bingman's Paperclipped extension.}
   URL         = "https://github.com/pgharts/trusty-clipped-extension"
