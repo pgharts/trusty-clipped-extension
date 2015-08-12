9b08e691f94822f2e164f8e0dcda576dc128f9c4
Eric Sipple <saalon@gmail.com>
2015-08-12 13:41:31 -0400

    For some reason, user_observer migration was trying to add created_at and updated_at on a table where it already existed.
    Removing those two column additions.

--- db/migrate/003_create_user_observer.rb
+++ db/migrate/003_create_user_observer.rb
@@ -2,8 +2,6 @@ class CreateUserObserver < ActiveRecord::Migration
   def self.up
     add_column :assets, :created_by, :integer
     add_column :assets, :updated_by, :integer
-    add_column :assets, :created_at, :datetime
-    add_column :assets, :updated_at, :datetime
   end
   
   def self.down
