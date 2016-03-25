aa6866d10a5e7ee2f3d650fd7464e87d8e6dd601
Brittany Martin <brittany.jill.martin@gmail.com>
2016-03-25 13:03:11 -0400

    Upgrade TrustyCMS and move to version 2.0.8

--- lib/trusty-clipped-extension.rb
+++ lib/trusty-clipped-extension.rb
@@ -1,5 +1,5 @@
 module TrustyCmsClippedExtension
-  VERSION     = "2.0.7"
+  VERSION     = "2.0.8"
   SUMMARY     = %q{Assets for TrustyCms CMS}
   DESCRIPTION = %q{Asset-management derived from Keith Bingman's Paperclipped extension.}
   URL         = "https://github.com/pgharts/trusty-clipped-extension"
