6f54dd31ba817b38f69f91e874c59e2845525434
danigirl329 <greaves@trustarts.org>
2016-05-12 10:03:50 -0400

    Updated clipped version and gemfile

--- lib/trusty-clipped-extension.rb
+++ lib/trusty-clipped-extension.rb
@@ -1,5 +1,5 @@
 module TrustyCmsClippedExtension
-  VERSION     = "2.0.12"
+  VERSION     = "2.0.13"
   SUMMARY     = %q{Assets for TrustyCms CMS}
   DESCRIPTION = %q{Asset-management derived from Keith Bingman's Paperclipped extension.}
   URL         = "https://github.com/pgharts/trusty-clipped-extension"
